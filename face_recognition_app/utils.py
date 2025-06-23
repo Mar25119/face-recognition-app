3e1c1c95a54965eadfef512bb3b497c69ae06d9b
Mar25119 <Zaradkaka@gmail.com>
2025-06-23 10:18:59 +0300

    Update utils.py

--- face_recognition_app/utils.py
+++ face_recognition_app/utils.py
@@ -4,14 +4,15 @@ import face_recognition
 import cv2
 from io import BytesIO
 from PIL import Image
-from face_db_manager import get_all_faces
+from face_db_manager import FaceDBManager
 
+# Создаем один экземпляр менеджера базы данных
+db = FaceDBManager()
 
 def load_known_data():
-    """Загружает из БД эмбеддинги и имена."""
     encodings = []
     names = []
-    rows = get_all_faces()
+    rows = db.get_all_faces()
     for row in rows:
         _, name, _, encoding_blob = row
         encoding = np.frombuffer(encoding_blob, dtype=np.float64)
@@ -19,9 +20,7 @@ def load_known_data():
         names.append(name)
     return encodings, names
 
-
 def add_new_face(frame, name):
-    """Добавляет новое лицо из кадра в БД."""
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     pil_image = Image.fromarray(image)
     img_byte_arr = BytesIO()
@@ -35,5 +34,4 @@ def add_new_face(frame, name):
         return
     encoding_blob = encoding[0].tobytes()
 
-    from face_db_manager import add_face
-    add_face(name, photo_blob, encoding_blob)
+    db.add_face(name, photo_blob, encoding_blob)
